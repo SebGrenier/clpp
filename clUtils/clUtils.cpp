7632e6797edb4d834f37ed696f09d9155eb69775
Sebastien Grenier <sebastien.grenier1@gmail.com>
2018-03-16 20:36:23 -0400

    Started cmake config

---
+++ clUtils/clUtils.cpp
@@ -0,0 +1,11 @@
+#include "clUtils.hpp"
+
+bool cl::init_library()
+{
+	return true;
+}
+
+bool cl::free_library()
+{
+	return true;
+}
---
+++ clUtils/clUtils.hpp
@@ -0,0 +1,10 @@
+#ifndef _CL_UTILS_HPP_
+#define _CL_UTILS_HPP_
+
+namespace cl {
+	bool init_library();
+	bool free_library();
+}
+
+
+#endif // _CL_UTILS_HPP_
